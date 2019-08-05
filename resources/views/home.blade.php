c0eb4e1840f180d73c7d594a1fecc507bdaa38bc
CalvinShashank <calvin.shashank@ithands.net>
2019-08-05 15:56:00 +0530

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,98 @@
+@extends('adminlte::page')
+@section('title', 'AdminLTE')
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+@section('content')
+    <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">New Members</span>
+                    <span class="info-box-number">{{count($users)}}</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">CPU Traffic</span>
+                    <span class="info-box-number">90<small>%</small></span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Likes</span>
+                    <span class="info-box-number">41,410</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <!-- fix for small devices only -->
+        <div class="clearfix visible-sm-block"></div>
+
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Sales</span>
+                    <span class="info-box-number">760</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+    </div>
+    <div class="box box-primary">
+        <div class="box-header with-border">
+            <h3 class="box-title">Recently Added Users</h3>
+
+            <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+            </div>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+            <ul class="products-list product-list-in-box">
+                @foreach($users as $user)
+                    <li class="item">
+                        <div class="product-img">
+                            <img src="/img/default.jpg" alt="User Image">
+                        </div>
+                        <div class="product-info">
+                            <a href="javascript:void(0)" class="product-title">{{ $user->name }}
+                                <span class="label label-success pull-right">$399</span></a>
+                            <span class="product-description">{{ $user->email }}</span>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+        <!-- /.box-body -->
+        <div class="box-footer text-center">
+            <a href="javascript:void(0)" class="uppercase">View All Users</a>
+        </div>
+        <!-- /.box-footer -->
+    </div>
+@stop
---
+++ resources/views/vendor/adminlte/social.blade.php
@@ -0,0 +1,7 @@
+<div class="auth-links">
+    <a href="{{ url('/login/github') }}" class="btn btn-github"><i class="fa fa-github"></i> Github</a>
+    <a href="{{ url('/login/twitter') }}" class="btn btn-twitter" class="btn btn-twitter"><i
+                class="fa fa-twitter"></i> Twitter</a>
+    <a href="{{ url('/login/facebook') }}" class="btn btn-facebook" class="btn btn-facebook"><i
+                class="fa fa-facebook"></i> Facebook</a>
+</div>
